928aa778bfca69a5e7be27522e124d5117adfccd
Sandeep Hidellarachchi <sandeeph.de96@gmail.com>
2025-07-15 14:05:49 +0200

    commited the app.py file for simple Q and A chatbot

---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Implement langsmith tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_PROJECT'] = 'Simple Q & A Chatbot with openAI'
+
+# Define prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ('system','You are a William Shakespeare. please response to user query in a way that William Shakespeare whould have answered'),
+        ('user','Question:{question}')
+    ]
+)
+
+
+def generate_response(question, api_key, llm, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm)
+    output_pareser = StrOutputParser()
+    chain = prompt|llm|output_pareser
+    answer = chain.invoke({'question'})
+    return  answer
+
+
+# Creat the Streamlit app
+
+
+# Title
+st.title('Questions and Answers Chatbot wiht openAI')
+
+# Create side bar
+st.sidebar.title('Settings')
+api_key = st.sidebar.text_input('Enter your OpenAI API KEY: ', type='password')
+
+# Create dropdown to select OpenAI Models
+llm = st.sidebar.selectbox("Select an OpenAI model", ['gpt-4o', 'gpt-4-turbo', 'gpt-4'])
+
+# Set temperateure (if its naer 1 the answaer is creative if near 0 willalways give same answae)
+temperature = st.sidebar.slider('Temperature', min_value=0.0, max_value=1.0, value=0.7)
+
+# Set tokens
+max_tokens = st.sidebar.slider('Max Tokens', min_value=50, max_value=300, value=150)
+
+
+# Main interface for user input
+st.write('GO ahead and ask any question')
+user_input = st.text_input('You:')
+
+
+if user_input:
+    response = generate_response(question=user_input,
+                                 api_key=api_key,
+                                 llm=llm,
+                                 temperature=temperature,
+                                 max_tokens=max_tokens)
+    st.write(response)
+else:
+    st.write('Please Enter your query')
